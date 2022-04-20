31d4c3946cbd90bae9fd3f8d03850ba3b3bedda4
Shankar2302 <94792409+Shankar2302@users.noreply.github.com>
2022-04-20 14:11:30 +0530

    Update streamlit_app.py
    provided an option to choose by the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ streamlit.header('🍑🍒Build Your Own Fruit Smoothie🍉🍊')
 import pandas as pd
 
 My_Fruit_List = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+My_Fruit_List = My_Fruit_list.set_index('Fruit')
+
 #let's put a picklist here so that customers can select the fruit they want to include in their smoothie
 streamlit.multiselect("Pick your choice of Fruit:", list(My_Fruit_List.index))
 
